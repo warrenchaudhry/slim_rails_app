81ec9bb20b9ecf2d2c9f722efc80059dc4f40ff8
Warren Chaudhry <warrenchaudhry@gmail.com>
2014-09-04 14:24:30 +0800

    adding custom title

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -8,8 +8,13 @@ describe "StaticPages" do
     it "should have the content 'Sample App'" do
       expect(page).to have_content('Sample App')
     end
-    it "should have the title 'Home'" do
-      expect(page).to have_title('Ruby on Rails Tutorial Sample App | Home')
+
+    it "should have the base title" do
+      expect(page).to have_title('Ruby on Rails Tutorial Sample App')
+    end
+
+    it "should not have a custom page title" do
+      expect(page).not_to have_title('| Home')
     end
   end
 
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,16 +1,24 @@
-# This file is copied to spec/ when you run 'rails generate rspec:install'
-ENV["RAILS_ENV"] ||= 'test'
-require File.expand_path("../../config/environment", __FILE__)
-require 'rspec/rails'
-require 'rspec/autorun'
+require 'rubygems'
+require 'spork'
+#uncomment the following line to use spork with the debugger
+#require 'spork/ext/ruby-debug'
 
-# Requires supporting ruby files with custom matchers and macros, etc,
-# in spec/support/ and its subdirectories.
-Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
+Spork.prefork do
+  ENV["RAILS_ENV"] ||= 'test'
+  require File.expand_path("../../config/environment", __FILE__)
+  require 'rspec/rails'
+  require 'rspec/autorun'
 
-# Checks for pending migrations before tests are run.
-# If you are not using ActiveRecord, you can remove this line.
-ActiveRecord::Migration.check_pending! if defined?(ActiveRecord::Migration)
+  # Requires supporting ruby files with custom matchers and macros, etc,
+  # in spec/support/ and its subdirectories.
+  Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
+
+  # Checks for pending migrations before tests are run.
+  # If you are not using ActiveRecord, you can remove this line.
+  ActiveRecord::Migration.check_pending! if defined?(ActiveRecord::Migration)
+
+end
+
 
 RSpec.configure do |config|
   # ## Mock Framework
