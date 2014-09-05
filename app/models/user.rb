5236fd5a42280ce61c367ae411b1f2543ced5af1
Warren Chaudhry <warrenchaudhry@gmail.com>
2014-09-05 13:50:08 +0800

    finished modeling users

---
+++ app/models/user.rb
@@ -0,0 +1,15 @@
+class User < ActiveRecord::Base
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  before_save { email.downcase! }
+  has_secure_password
+  validates :name, presence: true, length: {maximum: 50}
+  validates :email, presence: true,
+                    format: { with: VALID_EMAIL_REGEX },
+                    uniqueness: { case_sensitive: false }
+  validates :password, length: { minimum: 6}
+  validates_confirmation_of :password, if: lambda { |m| m.password.present? }
+
+
+
+end
+
--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -1,4 +1,4 @@
-require 'spec/helper'
+require 'spec_helper'
 
 describe ApplicationHelper do
 
