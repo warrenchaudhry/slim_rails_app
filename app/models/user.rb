19dcd038ed49c89638fd64c9c543326edbb24659
Warren Chaudhry <warrenchaudhry@gmail.com>
2014-09-18 14:43:04 +0800

    Finish signin and signout

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ActiveRecord::Base
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-  before_save { email.downcase! }
+  before_save { self.email = email.downcase }
+  before_create :create_remember_token
   has_secure_password
   validates :name, presence: true, length: {maximum: 50}
   validates :email, presence: true,
@@ -9,6 +10,20 @@ class User < ActiveRecord::Base
   validates :password, length: { minimum: 6}
   validates_confirmation_of :password, if: lambda { |m| m.password.present? }
 
+
+  def self.new_remember_token
+    SecureRandom.urlsafe_base64
+  end
+
+  def self.digest(token)
+    Digest::SHA1.hexdigest(token.to_s)
+  end
+
+  private
+
+    def create_remember_token
+      self.remember_token = User.digest(User.new_remember_token)
+    end
 
 
 end
